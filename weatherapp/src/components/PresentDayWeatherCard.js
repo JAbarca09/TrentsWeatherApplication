be5c1fddcc4a5403b228098123e829bd1580c57b
Jesse Abarca <JesseAbarca888@gmail.com>
2023-04-10 20:34:40 -0700

    Begin with initial set up of PresentDayWeatherCard

--- weatherapp/src/components/PresentDayWeatherCard.js
+++ weatherapp/src/components/PresentDayWeatherCard.js
@@ -1,5 +1,15 @@
-const PresentDayWeatherCard = () => {
+import React from "react";
+import classes from "./PresentDayWeatherCard.module.css";
 
+const PresentDayWeatherCard = (props) => {
+/* 
+recieves props regarding the current weather conditions and time!
+*/
+  return (
+    <div className={classes.mainCard}>
+        name
+    </div>
+  );
 };
 
 export default PresentDayWeatherCard;
--- weatherapp/src/components/SearchBar.js
+++ weatherapp/src/components/SearchBar.js
@@ -1,6 +1,8 @@
 import React, { useRef } from "react";
+import "./PresentDayWeatherCard";
 import classes from "./SearchBar.module.css";
 import emptyStar from "../assets/Icon_feather_star.png";
+import PresentDayWeatherCard from "./PresentDayWeatherCard";
 // import fullStar from "../assets/Icon_feather_starFilled.png";
 
 const SearchBar = () => {
@@ -13,22 +15,25 @@ const SearchBar = () => {
   };
 
   return (
-    <div className={classes.container}>
-      <div className={classes.imageContainer}>
-        <img className={classes.starImg} src={emptyStar} alt="empty star" />
+    <>
+      <div className={classes.container}>
+        <div className={classes.imageContainer}>
+          <img className={classes.starImg} src={emptyStar} alt="empty star" />
+        </div>
+        <form className={classes.form} onSubmit={handleFormSubmit}>
+          <input
+            className={classes.searchBar}
+            type="text"
+            placeholder="Search..."
+            ref={cityQueryRef}
+          />
+          <button className={classes.button} type="submit">
+            Search
+          </button>
+        </form>
       </div>
-      <form className={classes.form} onSubmit={handleFormSubmit}>
-        <input
-          className={classes.searchBar}
-          type="text"
-          placeholder="Search..."
-          ref={cityQueryRef}
-        />
-        <button className={classes.button} type="submit">
-          Search
-        </button>
-      </form>
-    </div>
+      <PresentDayWeatherCard />
+    </>
   );
 };
 
