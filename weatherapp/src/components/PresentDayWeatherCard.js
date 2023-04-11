40d313dd29e383599f23e0cde1f00fbac303e735
Jesse Abarca <JesseAbarca888@gmail.com>
2023-04-10 21:21:27 -0700

    Add layout to first row of main weather card

--- weatherapp/src/components/PresentDayWeatherCard.js
+++ weatherapp/src/components/PresentDayWeatherCard.js
@@ -1,13 +1,20 @@
 import React from "react";
 import classes from "./PresentDayWeatherCard.module.css";
+import emptyFavoriteStar from "../assets/Icon_feather_star_smol.png";
+import fullFavoriteStar from "../assets/Icon_feather_starfilledFavorited.png";
 
 const PresentDayWeatherCard = (props) => {
 /* 
 recieves props regarding the current weather conditions and time!
 */
   return (
-    <div className={classes.mainCard}>
-        name
+    <div className={classes.mainWeatherCard}>
+      <div className={classes.firstRow}>
+        <p>Stockton, US</p>
+        <img width="50px" height="50px" src={emptyFavoriteStar} alt="placeholder" />
+        <p>57 &deg;F</p>
+        <img width="50px" height="50px" src={fullFavoriteStar} alt="City Favorited" />
+      </div>
     </div>
   );
 };
