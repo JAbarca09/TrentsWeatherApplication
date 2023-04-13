23dedb4920b6346481323012100b71f2a3ace98f
Jesse Abarca <JesseAbarca888@gmail.com>
2023-04-12 17:29:10 -0700

    Format PresentDayWeatherCard.js code

--- weatherapp/src/components/PresentDayWeatherCard.js
+++ weatherapp/src/components/PresentDayWeatherCard.js
@@ -4,16 +4,26 @@ import emptyFavoriteStar from "../assets/Icon_feather_star_smol.png";
 import fullFavoriteStar from "../assets/Icon_feather_starfilledFavorited.png";
 
 const PresentDayWeatherCard = (props) => {
-/* 
+  /* 
 recieves props regarding the current weather conditions and time!
 */
   return (
     <div className={classes.mainWeatherCard}>
       <div className={classes.firstRow}>
         <p className={classes.stateAndCityTxt}>Stockton, US</p>
-        <img className={classes.weatherIcon} width="50px" height="50px" src={emptyFavoriteStar} alt="weather icon" />
+        <img
+          className={classes.weatherIcon}
+          width="50px"
+          height="50px"
+          src={emptyFavoriteStar}
+          alt="weather icon"
+        />
         <p className={classes.temperatureTxt}>57 &deg;F</p>
-        <img className={classes.starIcon} src={fullFavoriteStar} alt="City Favorited" />
+        <img
+          className={classes.starIcon}
+          src={fullFavoriteStar}
+          alt="City Favorited"
+        />
       </div>
     </div>
   );
