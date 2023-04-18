886181d9dc6516e57cbb36ae5e5339dd5a0f6ce3
Jesse Abarca <JesseAbarca888@gmail.com>
2023-04-17 22:05:30 -0700

    Set up container with mobile first media queries

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
+import classes from "./App.module.css";
 import "./App.module.css";
 
 import SearchBar from "./components/SearchBar";
 
 const App = () => {
   return (
-    <>
+    <div className={classes.container}>
       <SearchBar />
-    </>
+    </div>
   );
 }
 
