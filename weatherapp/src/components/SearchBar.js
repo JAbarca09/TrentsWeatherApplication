2b08ed73cc483a4da18be54727142d22e21689b6
Jesse Abarca <JesseAbarca888@gmail.com>
2023-04-18 15:11:26 -0700

    Revist the search bar

--- weatherapp/src/components/SearchBar.js
+++ weatherapp/src/components/SearchBar.js
@@ -1,7 +1,8 @@
 import React, { useRef } from "react";
 import "./PresentDayWeatherCard";
 import emptyStar from "../assets/Icon_feather_star.png";
-import PresentDayWeatherCard from "./PresentDayWeatherCard";
+import classes from "./SearchBar.module.css";
+// import PresentDayWeatherCard from "./PresentDayWeatherCard";
 // import fullStar from "../assets/Icon_feather_starFilled.png";
 
 const SearchBar = () => {
@@ -15,22 +16,38 @@ const SearchBar = () => {
 
   return (
     <>
-      <div>
-        <div>
-          <img src={emptyStar} alt="empty star" />
+        <div className={classes.container}>
+          <div className={classes["empty-star"]}>
+            <img src={emptyStar} width="50px" alt="empty star" />
+          </div>
+          <form className={classes["search-bar"]} onSubmit={handleFormSubmit}>
+            <input
+              className={classes.search}
+              id="search"
+              type="text"
+              placeholder="Search..."
+              ref={cityQueryRef}
+            />
+            <label className={classes.visuallyHidden} htmlFor="search">
+              Search bar
+            </label>
+            <button className={classes["search-btn"]} type="submit">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                id="search"
+              >
+                <g data-name="Layer 2">
+                  <path
+                    fill="#ffffff"
+                    d="m20.71 19.29-3.4-3.39A7.92 7.92 0 0 0 19 11a8 8 0 1 0-8 8 7.92 7.92 0 0 0 4.9-1.69l3.39 3.4a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42zM5 11a6 6 0 1 1 6 6 6 6 0 0 1-6-6z"
+                    data-name="search"
+                  ></path>
+                </g>
+              </svg>
+            </button>
+          </form>
         </div>
-        <form onSubmit={handleFormSubmit}>
-          <input
-            type="text"
-            placeholder="Search..."
-            ref={cityQueryRef}
-          />
-          <button type="submit">
-            Search
-          </button>
-        </form>
-      </div>
-      <PresentDayWeatherCard />
     </>
   );
 };
